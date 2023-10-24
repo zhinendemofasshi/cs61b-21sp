6f5649cae0aa7583901c0c47af1c6408d08d22b8
Ike Yang <70048185+zhinendemofasshi@users.noreply.github.com>
2023-10-24 17:15:15 +0800

    done with lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,132 @@
+package bstmap;
+
+import java.rmi.UnexpectedException;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>
+{
+    private BSTNode root;
+
+    private class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left, right;
+        private int size;
+
+        public BSTNode(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    public BSTMap() {
+    }
+
+    @Override
+    public void clear() {
+        root.size = 0;
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    public V get(BSTNode x, K key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        }
+        else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        else {
+            if (x.value == null) {
+                return (V) new Object();
+            }
+            return x.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    public int size(BSTNode x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    public BSTNode put(BSTNode x, K key, V value) {
+
+        if (x == null) {
+            return new BSTNode(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left =  put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        }
+        else {
+            x.value = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    public void PrintInOrder() {
+        if (root == null) {
+            return;
+        }
+        PrintInOrder(root);
+    }
+
+    public void PrintInOrder(BSTNode x) {
+        if (x == null) {
+            return;
+        }
+        PrintInOrder(x.left);
+        System.out.print(x.key + " ");
+        PrintInOrder(x.right);
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
