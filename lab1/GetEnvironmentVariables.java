37a25d9a2e2eb9a174d03ddc802fad981610f3ed
Ike Yang <70048185+zhinendemofasshi@users.noreply.github.com>
2023-12-29 15:58:32 +0800

    fix: revise the $REPO_DIR

--- lab1/GetEnvironmentVariables.java
+++ lab1/GetEnvironmentVariables.java
@@ -3,7 +3,7 @@ import java.util.Properties;
 public class GetEnvironmentVariables {
 
     //  Fill in the path to your sp21-s*** folder between the quotes
-    public static String REPO_DIR = "";
+    public static String REPO_DIR = "/Users/ikeyang/Desktop/code/cs61b-21sp";
 
     //  Fill in the path to your snaps-sp21-s*** folder between the quotes
     public static String SNAPS_DIR = "";
